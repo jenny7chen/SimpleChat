66f522e8ff83df4b84aff84decbf0253de276fee
Jenny Chen <jennychen@chenwanqide-Mac-mini.local>
2017-02-21 15:14:04 +0800

    add user model

---
+++ app/src/main/java/com/seveneow/simplechat/model/User.java
@@ -0,0 +1,8 @@
+package com.seveneow.simplechat.model;
+
+/**
+ * Created by jennychen on 2017/2/21.
+ */
+
+public class User {
+}
---
+++ app/src/main/java/com/seveneow/simplechat/utils/UserManager.java
@@ -0,0 +1,8 @@
+package com.seveneow.simplechat.utils;
+
+/**
+ * Created by jennychen on 2017/2/21.
+ */
+
+public class UserManager {
+}
